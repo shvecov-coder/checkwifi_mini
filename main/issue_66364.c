a6aeff3cafbc0a29cd27e75ff39fbf907f07f493
d.shvetsov <d.shvetsov@geoscan.aero>
2023-11-07 16:56:34 +0300

    added connect to RSSI

--- main/issue_66364.c
+++ main/issue_66364.c
@@ -7,13 +7,92 @@
 #include "esp_event.h"
 #include "nvs_flash.h"
 
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+#define ESP_WIFI_SSID ""
+#define ESP_MAXIMUM_RETRY 10
+#define ESP_WIFI_PASS "12345678"
 #define DEFAULT_SCAN_LIST_SIZE 10
 
-static const char * TAG = "ISSUE_66364";
+static int s_retry_num = 0;
+static const char * TAG = "issue_66364";
+static EventGroupHandle_t s_wifi_event_group;
 
-static void connect_to_SSID(const char * SSID)
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
 
+static void connect_to_SSID(uint8_t * ssid, const char * pass, int s_ssid, int s_pass)
+{
+	if (pass == NULL)
+	{
+		pass = ESP_WIFI_PASS;
+		s_pass = sizeof(ESP_WIFI_PASS);
+	}
+	s_wifi_event_group = xEventGroupCreate();
+
+	esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config;
+    memset(&wifi_config, 0, sizeof(wifi_config));
+    memcpy(&wifi_config.sta.ssid, ssid, s_ssid);
+    memcpy(&wifi_config.sta.password, pass, s_pass);
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    esp_wifi_connect();
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", ssid, pass);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", ssid, pass);
+    }
+    else
+    {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
 }
 
 static void connect_to_RSSI(void)
@@ -36,16 +115,12 @@ static void connect_to_RSSI(void)
 	esp_wifi_scan_start(NULL, true);
 	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
 	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
-	ESP_LOGI(TAG, "Total APs scanned = %u", ap_count);
-	for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < ap_count); i++) {
-		ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
-		ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
-	}
+	ESP_LOGI(TAG, "WIFI FOUNDED: %s\n", ap_info[0].ssid);
+	connect_to_SSID(ap_info[0].ssid, NULL, sizeof(ap_info[0].ssid), 0);
 }
 
 void app_main(void)
 {
-	// Initialize NVS
 	esp_err_t ret = nvs_flash_init();
 	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
 		ESP_ERROR_CHECK(nvs_flash_erase());
